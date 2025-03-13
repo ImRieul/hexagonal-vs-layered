a5f33f96e910ed7211d40cbafda4e9aa8e7ad16f
ImRieul <rieul.im@gmail.com>
2025-03-13 09:57:14 +0900

    프로젝트 초기 설정 파일 및 Gradle 관련 파일 추가: .gitattributes, .gitignore, build.gradle, gradlew, gradlew.bat, settings.gradle, gradle/wrapper/gradle-wrapper.properties, gradle/wrapper/gradle-wrapper.jar, 기본 애플리케이션 클래스 및 테스트 클래스 생성.

---
+++ src/main/java/com/example/hexagonalvslayered/HexagonalVsLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalvslayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalVsLayeredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalVsLayeredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hexagonalvslayered/HexagonalVsLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalvslayered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalVsLayeredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
